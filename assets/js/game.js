17ed331df117938b8f62e1983d62bfcdbe8363a6
J <torres.j.isaias@gmail.com>
2021-06-30 14:13:58 -0500

    added array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,16 @@
+// Game states
+// "WIN" - Player robot has defeated all enemy-robots
+//  * FIght all enemy-robots
+//  * Defeat each nemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+
 // var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -68,4 +71,4 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+// fight();
