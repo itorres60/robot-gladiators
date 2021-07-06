ce670f7526dd75d47174f71d44ed8d50a6d7629c
J <torres.j.isaias@gmail.com>
2021-07-06 14:05:40 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,7 @@ var enemyAttack = 12;
 
 console.log(enemyNames);
 console.log(enemyNames.length);
-console.log(enemyNames[0]);
-console.log(enemyNames[3]);
+
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -26,17 +25,18 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack -3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-    );
+      );
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -51,8 +51,9 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    // generate random damage based on enemy's attack power
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -74,7 +75,7 @@ var fight = function(enemyName) {
 var startGame = function() {
   // reset player stats
   playerHealth = 100;
-  playerAttack = 100;
+  playerAttack = 10;
   playerMoney = 10;
   for (var i = 0; i < enemyNames.length; i++) {
     // if player is still alive, keep fighting
@@ -86,7 +87,8 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
   
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
+      console.log(pickedEnemyName + " " + enemyHealth);
   
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
@@ -138,20 +140,31 @@ var shop = function() {
     "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
   );
   switch(shopOptionPrompt) {
+    case "REFILL": //new case
     case "refill":
-      window.alert("Refilling player's health by 20 for 7 dollars.");
-      // increase health and decreasede money
-      playerHealth = playerHealth + 20;
-      playerMoney = playerMoney - 7;
+      if (playerMoney < 7){
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        // increase health and decreasede money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      } else {
+        window.alert("You don't have enought money!");
+      }
       break;
 
+    case "UPGRADE": // new case
     case "upgrade":
-      window.alert("Upgrading player's attack by 6 for 7 dollars.");
-      // increase attack and decrease money
-      playerAttack = playerAttack + 6;
-      playerMoney = playerMoney - 7;
+      if (playerMoney < 7){
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        // increase attack and decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      } else {
+        window.alert("You don't have enough money!")
+      }    
       break;
-
+      
+    case "LEAVE": // new casee
     case "leave":
       window.alert("Leaving the store");
       //do nothing, so function will end
@@ -163,6 +176,13 @@ var shop = function() {
       shop();
       break;
   }
+};
+
+// function to generatre a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 
 // start the game wehn the page loads
